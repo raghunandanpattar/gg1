410b74fcd97fe469f1fe36cbf4b37ff9152a3674
raghunandanpattar <53566456+raghunandanpattar@users.noreply.github.com>
2023-04-13 17:02:01 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,63 @@
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIAYBUDLFUTXJNOPYA3"
+  secret_key = "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"
+}
+resource "aws_default_vpc" "default"{
+
+}
+
+
+resource "aws_security_group" "http_server_sg" {
+  name   = "http_server_sg"
+  vpc_id = aws_default_vpc.default.id
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+  tags = {
+    "name" = "http_server_sg"
+  }
+
+}
+resource "aws_instance" "http_Server" {
+  ami                    = "ami-06e46074ae430fba6"
+  key_name               = "default-ec2"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.http_server_sg.id]
+  subnet_id              = tolist(data.aws_subnet_ids.default_subnets.ids)[5]
+
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ec2-user"
+    private_key = file(var.aws_key_pair)
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum install httpd -y",
+      "sudo service httpd start",
+      "echo Hey ashutosh , this is my first ec2 instance  ${self.public_dns}| sudo tee /var/www/html/index.html"
+    ]
+  }
+}
+
